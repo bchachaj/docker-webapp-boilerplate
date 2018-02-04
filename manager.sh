f17e0ec0b07c044b798cafb616f4bf3befc4bff4
Ben Chachaj <benedict.chachaj@student.fairfield.edu>
2018-02-04 14:16:29 -0500

    sketch out manager script

--- manager.sh
+++ manager.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo 'ex privileges'
+
+
+#1.) Creating django project
+ #sudo docker-compose run web django-admin.py startproject composeexample .
+
+  #1.a) linux needs: sudo chown -R $USER:$USER .
+
+#2.) need to then wire database to postgres backend
+
+#3.) docker-compose up
+
+#4) trying bmf to ready and create migrations
+
+
+#) can remove with rm -rf django once docker down
